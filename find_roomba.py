8d3a2808f663a30e43bfca5d93eb7d612133a3a4
Dancinggremlin <malcolmcgrossman@gmail.com>
2018-02-27 09:31:10 -0600

    fixed artifacts from transfer

--- find_roomba.py
+++ find_roomba.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import cv2
 import numpy as np
 
@@ -18,24 +17,3 @@ def find_roomba():
 
 
 
-=======
-import cv2
-import numpy as np
-
-def find_roomba():
-	feed = cv2.VideoCapture(0)
-	while True:
-		_, frame = feed.read()
-		frame = cv2.resize(frame, (32,32), interpolation=cv2.INTER_CUBIC)
-		frame = np.array(frame)
-		r = frame[:,:,0].flatten()
-		g = frame[:,:,1].flatten()
-		b = frame[:,:,2].flatten()
-		image = np.array([list(r) + list(g) + list(b)], np.uint8)
-		"""
-		Insert neural network to evaluate image here
-		"""
-
-
-
->>>>>>> 64c1f092c4c555bd86471ff8f773743a60d239c1
--- image_grabber.py
+++ image_grabber.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import cv2
 import os
 
@@ -20,27 +20,4 @@ def main():
 	image_grabber(filename=filename)
 
 if __name__ == "__main__":
-=======
-import cv2
-import os
-
-def image_grabber(filename):
-	feed = cv2.VideoCapture(1)
-	num= 500
-	capture_frame = 1
-	frame_idx = 0
-	while num < 1000:
-		_, frame = feed.read()
-		if frame_idx % capture_frame== 0:
-			file = os.path.join(filename, 'img%d.JPEG' % num)
-			cv2.imwrite(filename=file, img=frame,
-						params= [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-			num += 1
-		frame_idx += 1
-def main():
-	filename = r'roomba_photos\roomba'
-	image_grabber(filename=filename)
-
-if __name__ == "__main__":
->>>>>>> 64c1f092c4c555bd86471ff8f773743a60d239c1
 	main()
